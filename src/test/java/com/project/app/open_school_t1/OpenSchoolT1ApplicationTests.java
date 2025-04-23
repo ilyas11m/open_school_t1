e015a94e2aa406df23d15c38f45cf494f4392358
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-04-23 22:53:56 +0300

    initial commit

---
+++ src/test/java/com/project/app/open_school_t1/OpenSchoolT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.app.open_school_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSchoolT1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
