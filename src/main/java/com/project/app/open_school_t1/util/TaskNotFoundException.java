19ac3bbe4fa036b0ac3c2fe574d7ad5ffbbd2a4a
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-04-28 15:58:48 +0300

    Obj was not found exception

---
+++ src/main/java/com/project/app/open_school_t1/util/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.app.open_school_t1.util;
+
+public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
