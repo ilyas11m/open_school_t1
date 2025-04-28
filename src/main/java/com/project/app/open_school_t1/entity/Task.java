e11e07bf72cc4006b3ee12077655a2f55e1e0769
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-04-28 16:02:19 +0300

    Task entity

---
+++ src/main/java/com/project/app/open_school_t1/entity/Task.java
@@ -0,0 +1,27 @@
+package com.project.app.open_school_t1.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "userid", nullable = false)
+    private Long userId;
+
+}
