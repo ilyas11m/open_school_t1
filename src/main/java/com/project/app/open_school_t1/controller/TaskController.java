f951ba86df3dc699544fa7cc09e6073757a2620d
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-05-03 23:03:49 +0300

    Add TaskMapper for converting between Task and TaskDTO

--- src/main/java/com/project/app/open_school_t1/controller/TaskController.java
+++ src/main/java/com/project/app/open_school_t1/controller/TaskController.java
@@ -2,16 +2,16 @@ package com.project.app.open_school_t1.controller;
 
 import com.project.app.open_school_t1.dto.TaskDTO;
 import com.project.app.open_school_t1.entity.Task;
+import com.project.app.open_school_t1.mapper.TaskMapper;
 import com.project.app.open_school_t1.service.inf.TaskService;
-import com.project.app.open_school_t1.util.TaskNotFoundException;
-
-import static com.project.app.open_school_t1.dto.TaskDTO.toEntity;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+
+import static com.project.app.open_school_t1.mapper.TaskMapper.toEntity;
 
 @RestController
 @RequestMapping("/tasks")
@@ -25,45 +25,35 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getTaskById(@PathVariable Long id) {
-        try {
-            TaskDTO task = TaskDTO.toDTO(taskService.getById(id));
-            return ResponseEntity.ok(task);
-        } catch (TaskNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    @ResponseStatus(value = HttpStatus.OK)
+    public TaskDTO getTaskById(@PathVariable Long id) {
+        return TaskMapper.toDTO(taskService.getById(id));
     }
 
     @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
     public List<TaskDTO> getAllTasks() {
         return taskService.getAllTasks()
                 .stream()
-                .map(TaskDTO::toDTO)
+                .map(TaskMapper::toDTO)
                 .toList();
     }
 
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     public TaskDTO addTask(@RequestBody TaskDTO taskDto) {
-        return TaskDTO.toDTO(taskService.addTask(toEntity(taskDto)));
+        return TaskMapper.toDTO(taskService.addTask(toEntity(taskDto)));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateTask(@PathVariable Long id, @RequestBody Task newTask) {
-        try {
-            TaskDTO updatedTask = TaskDTO.toDTO(taskService.update(id, newTask));
-            return ResponseEntity.ok(updatedTask);
-        } catch (TaskNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    @ResponseStatus(value = HttpStatus.OK)
+    public TaskDTO updateTask(@PathVariable Long id, @RequestBody Task newTask) {
+        return TaskMapper.toDTO(taskService.update(id, newTask));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteTask(@PathVariable Long id) {
-        try {
-            TaskDTO deletedTask = TaskDTO.toDTO(taskService.deleteTask(id));
-            return ResponseEntity.ok(deletedTask);
-        } catch (TaskNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
     }
 }
